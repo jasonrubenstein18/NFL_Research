346784aeab2db5ab3a4ea0b10822c0388f68ca7a
Jason Rubenstein <jasonr34@outlook.com>
2020-01-19 11:29:29 -0500

    Update NFL_Data.R

--- NFL_Data.R
+++ NFL_Data.R
@@ -18,23 +18,24 @@ pbp_2019 <- season_play_by_play(2019)
 
 pbp_data = bind_rows(pbp_2014, pbp_2015, pbp_2016, pbp_2017, pbp_2018, pbp_2019)
 
+# update path for your own machine
 write.csv(pbp_data,"~/Desktop/R/NFL/NFL_pbp_data.csv", row.names = FALSE)
 
 
-# remove QB rushes which tend to have more extreme impact on EPA
-non_qb_rush = pbp_data[!grepl('scramble', pbp_data$desc),]
+# # remove QB rushes which tend to have more extreme impact on EPA
+# non_qb_rush = pbp_data[!grepl('scramble', pbp_data$desc),]
 
-# filter data and find EPA over total rush attempts by season
-rushing_stats <- non_qb_rush %>% filter(PassAttempt == 0 & PlayType != "No Play" & 
-                                    !is.na(Rusher)) %>% group_by(Season, Rusher) %>% summarise(RushAttempt = n(), 
-                                     Total_EPA = sum(EPA, na.rm = TRUE), EPA_per_Att = Total_EPA/RushAttempt) %>% 
-                                     filter(RushAttempt >= 50)
+# # filter data and find EPA over total rush attempts by season
+# rushing_stats <- non_qb_rush %>% filter(PassAttempt == 0 & PlayType != "No Play" & 
+#                                     !is.na(Rusher)) %>% group_by(Season, Rusher) %>% summarise(RushAttempt = n(), 
+#                                      Total_EPA = sum(EPA, na.rm = TRUE), EPA_per_Att = Total_EPA/RushAttempt) %>% 
+#                                      filter(RushAttempt >= 50)
 
-# Plot EPA per rush attempt by season
-ggplot(rushing_stats, aes(x = EPA_per_Att, y = as.factor(Season))) + geom_joy(scale = 3, 
-                      rel_min_height = 0.01) + theme_light() + ylab("Season") + xlab("EPA per Rush Attempt") + 
-                      scale_y_discrete(expand = c(0.01, 0)) + scale_x_continuous(expand = c(0.01,
-                      0)) + ggtitle("The Shifting Distribution of EPA per Rush Attempt") + theme(plot.title = element_text(hjust = 0.5, 
-                      size = 16), axis.title = element_text(size = 16), axis.text = element_text(size = 16))
+# # Plot EPA per rush attempt by season
+# ggplot(rushing_stats, aes(x = EPA_per_Att, y = as.factor(Season))) + geom_joy(scale = 3, 
+#                       rel_min_height = 0.01) + theme_light() + ylab("Season") + xlab("EPA per Rush Attempt") + 
+#                       scale_y_discrete(expand = c(0.01, 0)) + scale_x_continuous(expand = c(0.01,
+#                       0)) + ggtitle("The Shifting Distribution of EPA per Rush Attempt") + theme(plot.title = element_text(hjust = 0.5, 
+#                       size = 16), axis.title = element_text(size = 16), axis.text = element_text(size = 16))
 
 
